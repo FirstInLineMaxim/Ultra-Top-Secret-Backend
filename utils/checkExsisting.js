b98e573cfbf1692ba395ceb472bd8b689ca69339
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-12-31 01:58:31 +0100

    types for messages

--- utils/checkExsisting.js
+++ utils/checkExsisting.js
@@ -4,12 +4,15 @@ const pg = require("../utils/db");
 async function userInfo(req, res, next) {
   console.log("Checking User");
   const { lastName, firstName, email, password, language } = req.body;
-  console.log(lastName, firstName, email, password, language)
+  console.log(lastName, firstName, email, password, language);
   //Checks if all Values are Provided
   if (!email || !password || !lastName || !firstName || !language)
     return res
       .status(400)
-      .json({ message: "You need to Provide all the Information" });
+      .json({
+        type: "info",
+        message: "You need to Provide all the Information",
+      });
 
   //first checks if the email is already in database
   const result = await pg("Users")
@@ -19,7 +22,7 @@ async function userInfo(req, res, next) {
   if (result.length !== 0)
     return res
       .status(303)
-      .json({ message: "User with that Email already Exist" });
+      .json({ type: "info", message: "User with that Email already Exist" });
 
   next();
 }
--- utils/checkUser.js
+++ utils/checkUser.js
@@ -5,13 +5,18 @@ async function loginUserInfo(req, res, next) {
   console.log("Checking User");
   console.log(req.body);
   const { email } = req.body;
-  if (!email || !req.body.password) return res.status(400).json({ message: "Email and Password is required" });
+  if (!email || !req.body.password)
+    return res
+      .status(400)
+      .json({ type: "info", message: "Email and Password is required" });
   const result = await pg("Users")
     .where("email", email)
     .select("password", "id")
     .catch((err) => console.log(err));
   if (result.length === 0)
-    return res.status(400).json({ message: "Invalid email or password" });
+    return res
+      .status(400)
+      .json({ type: "info", message: "Invalid email or password" });
   const { password, id } = result[0];
   req.body.user = id;
   req.body.hash = password;
