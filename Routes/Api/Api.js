a09d578d84e52b3853dc823a5a1bf825ba00b389
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2023-01-06 09:32:32 +0100

    fix /

--- Routes/Api/Api.js
+++ Routes/Api/Api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const pg = require("../../utils/db");
 
-router.route("task/all").get(async (req, res) => {
+router.route("/task/all").get(async (req, res) => {
   res.json(await pg.select("*").from("Task"));
 });
 module.exports = router;
