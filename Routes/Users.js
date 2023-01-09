cdee43e933c1677b528195a6ab09fba60dcf3c02
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2023-01-09 20:35:09 +0100

    fix ?

--- Routes/Users.js
+++ Routes/Users.js
@@ -1,25 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const pg = require("../utils/db");
-const { route } = require("./Home");
 
 router
   .route("/")
-    //Returns all user
+  //Returns all user
   .get(async (req, res) => {
     res.json(await pg("Users").select("*"));
   })
   .post(async (req, res) => {});
 
-
-
-  router
+router
   .route("/profile")
-    //Returns Specific user
-  .get(async(req,res)=> {
-    const {user} = req
-    if(user) return res.json(await pg("Users").select("*").where("id",user))
-    return
-  })
+  //Returns Specific user
+  .get(async (req, res) => {
+    const { user } = req;
+    if (user) return res.json(await pg("Users").select("*").where("id", user));
+    return;
+  });
 
 module.exports = router;
