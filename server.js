928c086075a9804e78ed37f34295f79eb00def55
Quantumminded <lukkino31@gmail.com>
2023-01-18 12:31:04 +0100

    change the data in the staip

--- server.js
+++ server.js
@@ -67,8 +67,8 @@ app.get("*", function (req, res) {
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
 
 const storeItems = new Map([
-  [1, { priceInCents: 10000, name: "Learn React Today" }],
-  [2, { priceInCents: 20000, name: "Learn CSS Today" }],
+  [1, { priceInCents: 10000, name: "Translator" }],
+  [2, { priceInCents: 25000, name: "Translator" }],
  
 ])
 
